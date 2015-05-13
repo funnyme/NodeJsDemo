cd46c091726caa042fa309bb4211288b5b8cb31f
funnyme <roshni.dhaval@gmail.com>
2015-05-13 00:03:17 -0700

    demo

---
+++ app.js
@@ -0,0 +1,20 @@
+function prepareOrder(ordernumber){
+console.log('Prepare my order....');
+
+cookOrder(function(){
+console.log('Order  '+ordernumber+' Ready for pickup....');
+});
+};
+
+function cookOrder(callback){
+setTimeout(callback, 5000);
+};
+
+
+prepareOrder(1);
+prepareOrder(2);
+prepareOrder(3);
+prepareOrder(4);
+prepareOrder(5);
+prepareOrder(6);
+
